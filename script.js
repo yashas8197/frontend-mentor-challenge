159fe98ecb6f66794d49ecd84b0cab95f5d8e9bf
yashas8197 <130334248+yashas8197@users.noreply.github.com>
2023-05-08 08:11:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let ctitle = document.getElementsByClassName("calcu")[0]
+// ctitle.style.color = "red"
+
+// let dtitle = document.getElementById("kar")
+// dtitle.style.color = "blue"
+
+let etitle = document.querySelectorAll(".kar")
+// etitle[0].style.color = "blue"
+etitle[1].style.color = "red"
+etitle[2].style.color = "purple"
+etitle[3].style.color = "green"
+console.log(etitle);
+
+let ftitle = document.querySelector(".kar")
+ftitle.style.color = "blue"
+
